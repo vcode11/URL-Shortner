4bfe9e51c7daf23a571e12c46631c2dd1f72822b
Vishal Mishra <vishalm434@gmail.com>
2019-03-22 01:18:19 +0530

    Update staticdirs

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -108,5 +108,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 LOGIN_URL = '/login'
 django_heroku.settings(locals())
